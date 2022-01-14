f5c42549fe15e9a512cd26b1a92bf6b7e30b2e5b
mihhhha <mihhhha@gmail.com>
2022-01-14 12:26:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+ale=71
+age=int(input('skolko vam let? '))
+
+after20=age+20
+print('cherez 20 let vam budet', after20)
+
+alive=ale-age
+print('po statistike vam ostalos', alive)
+
+count=144000000
+all_alive=count*ale
+print('srednee vremya zhizni vse h ludei', all_alive)
+
+live_part=age/ale
+print('chast prozhitoi zhizni', live_part)
